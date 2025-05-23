13c3fbc0531fa912d840a87cfdeb4f174f68106c
gcruzbull <153864295+gcruzbull@users.noreply.github.com>
2025-05-23 21:54:04 +0000

    mejoras de la imagen y estructura del home y navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,20 @@
 import { Link } from "react-router-dom";
+import React from "react";
 
 export const Navbar = () => {
 
 	return (
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
-				<Link to="/">
+				{/* <Link to="/">
 					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
+				</Link> */}
 				<div className="ml-auto">
 					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
+						<button className="btn btn-success">Add new contact</button>
 					</Link>
 				</div>
-			</div>
+			</div>		
 		</nav>
-	);
+					);
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,36 @@ import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 
 export const Home = () => {
 
-  const {store, dispatch} =useGlobalReducer()
+	const { store, dispatch } = useGlobalReducer()
 
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
+			<h1>Contact List</h1>
+			{/* contenedor de las cards */}
+			<div>
+				{/* card */}
+				<div className="card mb-3" style={{maxWidth: "540px"}}>
+					<div className="row g-0">
+
+						<div className="col-md-3">
+							<img src="..." className="img-fluid rounded-start" alt="..."/>
+						</div>
+						<div className="col-md-6">
+							<div className="card-body">
+								<h5 className="card-title">Juan Perez</h5>
+								<p className="card-text">This is a wider </p>
+							</div>
+						</div>
+
+
+						<div className="col-md-3">
+							<button>Editar</button>
+							<button>Borrar</button>
+						</div>
+
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }; 
